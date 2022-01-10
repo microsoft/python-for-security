f15a0482e92f6f019dabdb53713013cb4df3ecc8
bmrodrigues <rodrigues.bruno@microsoft.com>
2022-01-10 09:42:50 +0000

    removed sensitive test data

--- MDE2TEAMS/app.py
+++ MDE2TEAMS/app.py
@@ -13,14 +13,10 @@ args = parser.parse_args()
 
 #Login token
 try:
-    #tenantid = input('Please enter your Tenant ID: ')
-    tenantid = '75b3e7f6-2e24-4fb0-8e84-ec9e587c8d4c'
-    #appid = input('Please enter your Application ID: ')
-    appid = '63b23291-8e11-4e1b-b102-9e5ad098af6e'
-    #secret = getpass.getpass('Please enter your Application Secret:')
-    secret = '4_U_S93E5MEYyomdw-Y1lV4.Ckw8y30l-1'
-    #webhook = getpass.getpass('Please enter your webhook from Teams: ')
-    webhook = 'https://microsoft.webhook.office.com/webhookb2/148071e4-ac99-4cd2-bfee-3571d8ef6bb9@72f988bf-86f1-41af-91ab-2d7cd011db47/IncomingWebhook/922241bcd32c41ec8b2f4f448b8da4ff/3171f984-4cb1-43cb-87a0-235e65fc8c84'
+    tenantid = input('Please enter your Tenant ID: ')
+    appid = input('Please enter your Application ID: ')
+    secret = getpass.getpass('Please enter your Application Secret:')
+    webhook = getpass.getpass('Please enter your webhook from Teams: ')
     new_token = gettoken.GetToken(tenantid, appid, secret)
     new_token.gettoken()
     token = new_token.aadToken
